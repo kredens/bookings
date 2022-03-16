36fabe31ee4dbfd0c6ecc94dddc68ba30e7e7cbb
Kelvin Kredens <kredens@gmail.com>
2022-03-16 17:52:28 -0300

    html files...

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,8 +18,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	// para os arquivos estáticos
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	// ---
 
 	return mux
 }
